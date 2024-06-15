05c4371025b1cb41b1fbd6c3f212e3ff49922d11
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-06-15 11:08:41 +0200

    add

--- modules/module 15/task_manager_project/lib/ui/screens/new_task_screen.dart
+++ modules/module 15/task_manager_project/lib/ui/screens/new_task_screen.dart
@@ -17,23 +17,36 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: profileAppBar(),
-      body:const Column(
+      body: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Card(
-                child: Column(
-                  children: [
-                    Text("12"),
-                    Text("New Task")
-                  ],
-                ),
-              ),
+              Expanded(child: taskSummaryCard("12","New Task")) ,
+              Expanded(child: taskSummaryCard("10","Completed")) ,
+              Expanded(child: taskSummaryCard("12","In Progress")) ,
+              Expanded(child: taskSummaryCard("01","Cancel")) ,
             ],
-          )
+          ),
         ],
       ),
     );
   }
-
+Card taskSummaryCard(number,taskName){
+    return Card(
+      color: Colors.white,
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Text(
+                number ,
+                style:Theme.of(context).textTheme.titleLarge),
+            Text(taskName,style:Theme.of(context).textTheme.labelMedium)
+          ],
+        ),
+      ),
+    );
+}
 }
