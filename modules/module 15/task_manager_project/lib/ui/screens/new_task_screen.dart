90f85b1f5dfbdcf0e95b960105b849ae08545887
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-06-15 10:26:47 +0200

    add

--- modules/module 15/task_manager_project/lib/ui/screens/new_task_screen.dart
+++ modules/module 15/task_manager_project/lib/ui/screens/new_task_screen.dart
@@ -16,10 +16,24 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("New Task"),
-        leading: const CircleAvatar(
-          radius: 12,
-          child: NetworkCachedImage(url: '', fit: BoxFit.fill, ),
+        title:const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Tasnim Islam Raisa",
+            style: TextStyle(
+              color: Colors.white,
+                fontSize: 18,fontWeight: FontWeight.w600),),
+            Text("islamtasnim65@gmail.com",
+              style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w400),),
+      ],
+        ),
+        elevation: 3,
+        leading:const Padding(
+          padding:  EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 12,
+            child: NetworkCachedImage(url: '', fit: BoxFit.fill, ),
+          ),
         ),
         backgroundColor: AppColors.themeColor,
       ),
