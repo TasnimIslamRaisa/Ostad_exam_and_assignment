9fda2519d16061eaf3bc6b8f67c3e19cdefe0d31
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-09-10 04:58:30 +0600

    dlt

--- modules/e_commers_app/lib/presentation/ui/utils/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class AppColors{
-  static const Color themeColor = Color(0xFF07ADAE);
-  static const Color bodyLargeColor = Colors.grey;
-}
--- modules/e_commers_app/lib/presentation/ui/utils/assets_path.dart
+++
@@ -1,4 +0,0 @@
-class AssetsPath{
-  static const String _images='assets/images';
-  static const String appLogo='$_images/logo.svg';
-}
