b372c307ce8ef98eaec0cd3dc132144492fffcf1
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-09-23 02:28:53 +0600

    worked with firebase notification

--- modules/test_project_with_firebase/lib/fire_base_notification_service.dart
+++ modules/test_project_with_firebase/lib/fire_base_notification_service.dart
@@ -1,10 +1,13 @@
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-class FireBaseNotification{
-  final FirebaseMessaging _firebaseMessaging=FirebaseMessaging.instance;
+class FireBaseNotification {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
   FireBaseNotification._();
   static final FireBaseNotification instance = FireBaseNotification._();
-  Future<void> initialize()async{
+
+  Future<void> initialize() async {
     NotificationSettings settings = await _firebaseMessaging.requestPermission(
       alert: true,
       announcement: false,
