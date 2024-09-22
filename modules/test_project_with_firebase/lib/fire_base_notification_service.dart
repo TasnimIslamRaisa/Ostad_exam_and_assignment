74573c967e354d8e564592aa09fc764520e6636b
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-09-23 02:18:16 +0600

    add

---
+++ modules/test_project_with_firebase/lib/fire_base_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FireBaseNotification{
+  final FirebaseMessaging _firebaseMessaging=FirebaseMessaging.instance;
+  FireBaseNotification._();
+  static final FireBaseNotification instance = FireBaseNotification._();
+  Future<void> initialize()async{
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    print('User granted permission: ${settings.authorizationStatus}');
+  }
+}
