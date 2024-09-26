2b16b87c7430457aceea6a7cf457131639d480f9
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-09-27 02:04:03 +0600

    geo get curren location

--- modules/test_project_with_firebase/lib/GeoLocator/geo_locator_home_screen.dart
+++ modules/test_project_with_firebase/lib/GeoLocator/geo_locator_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class GeoLocatorHomeScreen extends StatefulWidget {
   const GeoLocatorHomeScreen({super.key});
@@ -6,8 +7,10 @@ class GeoLocatorHomeScreen extends StatefulWidget {
   @override
   State<GeoLocatorHomeScreen> createState() => _GeoLocatorHomeScreenState();
 }
+//TODO:  to use this package we have to alow some permission to our .xml file
 
 class _GeoLocatorHomeScreenState extends State<GeoLocatorHomeScreen> {
+  Position? currentPosition;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +20,53 @@ class _GeoLocatorHomeScreenState extends State<GeoLocatorHomeScreen> {
         backgroundColor: Colors.pink,
         foregroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text("Current Location"),
+      body: Center(
+        child: Text("Current Location : $currentPosition"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _getCurrenLocation();
+        },
+        child: const Icon(Icons.location_history),
       ),
     );
   }
+
+  Future<void> _getCurrenLocation() async {
+    //TODO: check permission,ask permission
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.always ||
+        permission == LocationPermission.whileInUse) {
+      //TODO:Check GPS Service,ask to on
+      final bool isEnable = await Geolocator.isLocationServiceEnabled();
+      if (isEnable) {
+        //TODO: Get Current Location
+        currentPosition = await Geolocator.getCurrentPosition(
+          locationSettings : const LocationSettings(
+            accuracy: LocationAccuracy.best,
+            distanceFilter: 100,
+            timeLimit: Duration(seconds: 30),
+          ),
+        );
+
+        setState(() {});
+      } else {
+        // on gps service
+        Geolocator.openAppSettings();
+      }
+    } else {
+      if (permission == LocationPermission.deniedForever) {
+        Geolocator.openLocationSettings();
+        return;
+      }
+      LocationPermission requestedPermission =
+      await Geolocator.requestPermission();
+      if (requestedPermission == LocationPermission.always ||
+          requestedPermission == LocationPermission.whileInUse) {
+        _getCurrenLocation();
+      }
+    }
+  }
+
+
 }
