08ed5c3ef32f8d39e629f83ea728ccae643ddc1b
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-10-28 00:10:43 +0600

    web view controller

--- modules/e_commerce_app/lib/presentation/ui/screens/webview_screen.dart
+++ modules/e_commerce_app/lib/presentation/ui/screens/webview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({super.key, required this.link});
